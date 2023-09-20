91d4bc6bfac53ea1a98710747a7ed7cae7285954
diegocallegari96 <warsindustry@gmail.com>
2023-09-20 15:45:42 +0200

    resetbutton

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
 import './App.css'
-import FruitCard from "./components/FruitCard/FruitCard.jsx";
 import FruitsCounter from "./components/FruitCounter/FruitCounter.jsx"
 
 
 function App() {
 
+    const [bananasCount, setBananasCount] = useState(0);
+    const [strawberriesCount, setStrawberriesCount] = useState(0);
+    const [applesCount, setApplesCount] = useState(0);
+    const [kiwisCount, setKiwisCount] = useState(0);
+
+    const resetCounts = () => {
+        setBananasCount(0);
+        setStrawberriesCount(0);
+        setApplesCount(0);
+        setKiwisCount(0);
+    };
+
+
   return (
-    <>
+    <div>
         <h1>Fruitmand bezorgservice</h1>
-        <FruitCard textlable="&#127827; Aardbeijen"
-
+        <FruitsCounter fruittype="&#127827; Aardbeijen"
+                       count={strawberriesCount}
+                       setCount={setStrawberriesCount}
         />
-        <FruitsCounter value="bananen"/>
-        <FruitCard textlable="&#127820; Bananen"
-
+        <FruitsCounter fruittype="&#127820; Bananen"
+                       count={bananasCount}
+                       setCount={setBananasCount}
         />
-        <FruitCard textlable="&#127823; Appels"
-
+        <FruitsCounter fruittype="&#127823; Appels"
+                       count={applesCount}
+                       setCount={setApplesCount}
         />
-        <FruitCard textlable="&#129373; Kiwi's"
-
+        <FruitsCounter fruittype="&#129373; Kiwi's"
+                       count={kiwisCount}
+                       setCount={setKiwisCount}
         />
-    </>
-  )
+        <button onClick={resetCounts}>Reset</button>
+    </div>
+
+  );
 }
 
-export default App
+export default App;
--- src/components/FruitCounter/FruitCounter.jsx
+++ src/components/FruitCounter/FruitCounter.jsx
@@ -3,5 +3,25 @@ import React, { useState } from 'react';
 function FruitsCounter(props) {
     const [fruitCount, setFruitCount] =useState(0);
 
-    const increaseCount = ()
+    const increaseCount = () => {
+        setFruitCount(fruitCount + 1);
+    };
+
+    const decreaseCount = () => {
+        if (fruitCount > 0) {
+            setFruitCount(fruitCount - 1);
+        }
+    };
+
+
+    return (
+        <div>
+            <span>{props.fruittype}</span>
+            <button onClick={increaseCount}>+</button>
+            <span>{fruitCount}</span>
+            <button onClick={decreaseCount}>-</button>
+        </div>
+    )
 }
+
+export default FruitsCounter;
